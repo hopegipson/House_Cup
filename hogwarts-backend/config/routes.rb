64241aac058b485af563cab9339bb9161578b843
hopegipson <gipson.hope@gmail.com>
2021-01-26 16:06:58 -0600

    Created basic models

--- hogwarts-backend/config/routes.rb
+++ hogwarts-backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :usernames
+  resources :houses
   get '/test', to: 'application#test'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
