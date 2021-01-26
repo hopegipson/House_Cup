64fbbb0e9b3f379b27a4126220ecd39b0857f106
hopegipson <gipson.hope@gmail.com>
2021-01-26 16:24:38 -0600

    Seeding data and more connections

--- hogwarts-backend/app/models/house.rb
+++ hogwarts-backend/app/models/house.rb
@@ -1,2 +1,4 @@
 class House < ApplicationRecord
+    has_many :users
+
 end
--- hogwarts-backend/db/seeds.rb
+++ hogwarts-backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'securerandom'
+
+User.delete_all
+House.delete_all
+ 
+houses_name = [
+  'Gryffindor',
+  'Ravenclaw',
+  'Slytherin',
+  'Hufflepuff'
+]
+
+hogwarts_house_collection = []
+ 
+houses_name.each do |name|
+    hogwarts_house_collection << House.create(name: name)
+end
+
+hogwarts_house_collection.each do |house|
+    house_size = (SecureRandom.random_number(6) + 1).floor
+    (1..house_size).each do |poke|
+        username = Faker::Name.first_name
+        User.create(username: username, house_id: house.id)
+      end
+    end
